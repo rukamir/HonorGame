674e4a54693da54c5aeb595677ee70a73c0b9975
rukamir <jimmylroland@gmail.com>
2014-01-29 16:20:06 -0600

    corrected errors

--- src/com/Honor/libs/Audio.java
+++ src/com/Honor/libs/Audio.java
@@ -12,5 +12,20 @@ public class Audio
 	public static final String BUTTON = "buttton";
 	
 	public static final String BACKGROUND = "background";
-
+	public static final String COIN = "coin";
+	public static final String FIREBALL = "fireball";
+	public static final String JUMP = "jump";
+	public static final String KICK = "kick";
+	public static final String NEWLIFE ="newlife";
+	public static final String POWERDOWN = "powerdown";
+	public static final String POWERUP = "powerup";
+	public static final String CHECKPOINT = "checkpoint";
+	public static final String THUD = "thud";
+	public static final String WIND = "wind";
+	public static final String SWIMUP = "swimup";
+	public static final String SWIMDOWN = "swimdown";
+	public static final String ENEMY_DIE = "enemydie";
+	public static final String LEVER = "lever";
+	public static final String MARIO_DIE = "mariodie";
+	public static final String END_LEVEL = "endlevel";
 }
